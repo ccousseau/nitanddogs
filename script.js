50f65548f26ef7e98b8b2e4d1565384a18881781
ccousseau <colin.cousseau@reseau.sncf.fr>
2026-01-12 21:22:15 +0100

    ajustments

--- script.js
+++ script.js
@@ -4,10 +4,27 @@ window.onbeforeunload = function () {
 
 document.addEventListener('DOMContentLoaded', function() {
     let nav = document.querySelector('nav');
-    let navLinks = nav.querySelector('.nav-links');
+    let navLinks = nav.querySelector('.navliens');
     let hamburgerElement = nav.querySelector('.hamburger');
     hamburgerElement.addEventListener('click', function() {
         navLinks.classList.toggle('active');
     });
 });
 
+function lightMode() {
+   let element = document.body;
+   element.classList.toggle("lightmode"); 
+   // applique ou retire la classe "lightmode" au body
+
+   let bouton = document.getElementById("darklight");
+    if (bouton.innerHTML === "Light") {
+        bouton.innerHTML = "Dark";
+        bouton.style.backgroundColor = "black"
+        bouton.style.color = "white";
+    } else {
+        bouton.innerHTML = "Light";
+        bouton.style.backgroundColor = "white"
+        bouton.style.color = "black";
+    }
+   
+}
